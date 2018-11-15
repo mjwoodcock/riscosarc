61359867656ed02df7af73e725fd4ed6960ea4e9
James Woodcock <james_woodcock@yahoo.co.uk>
2018-11-15 17:53:25 +0000

    Test garbled archives.
    ArcFS archives fail.

--- testfiles/test.sh
+++ testfiles/test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 while read -r src dest; do
-	java riscosarc -x $src
+	passopt=""
+	if `echo $src | grep "_g," > /dev/null 2>&1`; then
+		passopt="-gpassword"
+	fi
+	java riscosarc -x $passopt $src
 	if [ ! -e $dest ]; then
 		echo "$src failed"
 		echo "$dest does not exist."
-	fi
-
-	if ! diff $dest source/19337.txt > /dev/null 2>&1; then
+	elif ! diff $dest source/19337.txt > /dev/null 2>&1; then
 		echo "$src failed"
 	fi
 
